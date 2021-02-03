9c05c1573421d291f19e73ee86d05bef9ff47f4d
lkakani27 <60006954+lkakani27@users.noreply.github.com>
2021-02-03 00:37:47 -0500

    Update Kubernetes-Installation.sh

--- Kubernetes-Installation.sh
+++ Kubernetes-Installation.sh
@@ -22,6 +22,15 @@ systemctl status docker
 
 ## Add the Kubernetes repo ##
 cat << EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
 
 ## Install Kubernetes ##
 yum install -y kubelet kubeadm kubectl
